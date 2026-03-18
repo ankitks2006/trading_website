8a691c2dc6b6735c0875518bc579a9c2f9e145be
Ankit <ankit@gmail.com>
2026-03-19 04:02:11 +0530

    final change1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-   baseURL: 'https://trading-website-flax.vercel.app/api',
+  //  baseURL: 'https://trading-website-flax.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(
